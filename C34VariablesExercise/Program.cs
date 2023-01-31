7586ea59f089ea91cf5dfe7437fece12d1770cf4
Amoriss <amoriss@protonmail.com>
2023-01-31 10:36:27 -0600

    initial commit

---
+++ C34VariablesExercise/Program.cs
@@ -0,0 +1,17 @@
+﻿
+
+string randy;
+randy = "name";
+
+int numberOfCats = 6;
+char exaclamationPoint = '!';
+bool allergicToCats = true;
+double amountOfBabies = 5;
+decimal saleAmount = 55.50m;
+
+Console.WriteLine($"Randy is the {randy} of our fictional character. He has {numberOfCats} \n" +
+    $"cats{exaclamationPoint} However, he's allergic-- it's {allergicToCats}. One of the cats just had {amountOfBabies} babies. \n" +
+    $"Randy is making a sale to get rid of cats so he can get a girlfriend. Each cat costs ${saleAmount}. \n" +
+    $"With that he can pay for his date.");
+Console.WriteLine();
+Console.WriteLine("Randy has a date next week. Success!");
